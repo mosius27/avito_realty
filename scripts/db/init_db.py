6239e4582e82a67ca7162567b5238716eadbb11b
mosius27 <mosius27@yandex.ru>
2022-08-08 07:43:43 +0300

    .

--- scripts/db/init_db.py
+++ scripts/db/init_db.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine, MetaData, Table, FetchedValue, inspect, Column, Integer
+from sqlalchemy import create_engine, MetaData, Table, FetchedValue, inspect, Column, Integer, Text
 from sqlalchemy.orm import sessionmaker, Query, query
-from sqlalchemy.sql import exists
+import migrate.changeset as migrate
 from db_settings import settings
 import db_model as model
 
@@ -13,5 +13,6 @@ if settings.DATABASE_TABLE_NAME not in inspector.get_table_names():
     model.table.metadata.create_all(engine)
 
 table = Table(settings.DATABASE_TABLE_NAME, meta, autoload=True)
-
+col = Column('test', Text)
+migrate.create_column(col, table=table)
 ...
